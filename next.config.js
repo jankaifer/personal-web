48fd5de3c8583a4e14cfe8c181ace69efe20b233
Jan Kaifer <jan@kaifer.cz>
2021-06-27 00:23:31 +0200

    installed mdx

--- next.config.js
+++ next.config.js
@@ -1,24 +1,31 @@
 const withImages = require("next-images");
+const withMDX = require("@next/mdx")();
 
-module.exports = withImages({
-  webpack: (config) => ({
-    ...config,
-    module: {
-      ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.md$/,
-          loader: "emit-file-loader",
-          options: {
-            name: "dist/[path][name].[ext]",
-          },
-        },
-        {
-          test: /\.md$/,
-          loader: "raw-loader",
-        },
-      ],
+module.exports = withMDX(
+  withImages({
+    images: {
+      domains: ["www.gravatar.com"],
     },
-  }),
-});
+    pageExtensions: ["ts", "tsx", "mdx"],
+    webpack: (config) => ({
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.md$/,
+            loader: "emit-file-loader",
+            options: {
+              name: "dist/[path][name].[ext]",
+            },
+          },
+          {
+            test: /\.md$/,
+            loader: "raw-loader",
+          },
+        ],
+      },
+    }),
+  })
+);
