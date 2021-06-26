95b4b26e8ae970b2308613c5bb3542fffd5090f1
Jan Kaifer <jan@kaifer.cz>
2021-06-27 00:22:11 +0200

    Added gravatar

--- Utils/misc.ts
+++ Utils/misc.ts
@@ -1,3 +1,5 @@
+import md5 from "crypto-js/md5";
+
 export const transformArrayToObjectByField = <
   T extends object,
   K extends keyof T
@@ -5,3 +7,9 @@ export const transformArrayToObjectByField = <
   objs: T[],
   field: K
 ) => Object.fromEntries(objs.map((obj) => [obj[field], obj]));
+
+export const getGravatarSrc = (email: string) => {
+  const hash = md5(email.trim().toLowerCase());
+  const gravatarUri = `https://www.gravatar.com/avatar/${hash}`;
+  return gravatarUri;
+};
