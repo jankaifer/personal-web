66fcbc0abd032201ebdec09e4583c671fb89f7ee
Jan Kaifer <jan@kaifer.cz>
2021-06-27 00:29:03 +0200

    fixed mdx issues

--- @types/mdx.d.ts
+++ @types/mdx.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="@mdx-js/loader" />
-
-declare module "@next/mdx" {
-  export const MDXProvider: (props: {
-    children: React.ReactNode;
-    components: Record<string, (props: any) => JSX.Element>;
-  }) => JSX.Element;
-}
