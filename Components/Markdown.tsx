ed1f6495e59f290a5ff5e3f5a464ee8141e107a8
Jan Kaifer <jan@kaifer.cz>
2021-08-12 12:41:55 +0200

    removed html support from markdown

--- Components/Markdown.tsx
+++ Components/Markdown.tsx
@@ -5,7 +5,7 @@ type ReactMarkdownProps = React.ComponentProps<typeof ReactMarkdown>;
 type Props = Required<Pick<ReactMarkdownProps, "children">>;
 
 const Markdown = ({ children, ...rest }: Props) => (
-  <ReactMarkdown allowDangerousHtml children={children} {...rest} />
+  <ReactMarkdown children={children} {...rest} />
 );
 
 export default Markdown;
