49288c2d11a501d374540d6a85b17f7c0966b706
Jan Kaifer <jan@kaifer.cz>
2021-08-29 18:33:06 +0200

    added enable javascript banners

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import { MDXProvider } from "@mdx-js/react";
+import { range } from "fp-ts/lib/ReadonlyNonEmptyArray";
 import { AppProps } from "next/app";
-import { createGlobalStyle, css, ThemeProvider } from "styled-components";
+import styled, {
+  createGlobalStyle,
+  css,
+  keyframes,
+  ThemeProvider,
+} from "styled-components";
 
 import CodeBlock from "Components/CodeBlock";
 import Layout from "Components/Layout";
@@ -78,10 +84,91 @@ const App = ({ Component, pageProps }: AppProps) => {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <MDXProvider components={components}>
+        <NoScriptBanners />
         <Component {...pageProps} />
       </MDXProvider>
     </ThemeProvider>
   );
 };
+
+const NoScriptBanners = () => {
+  const messages = [
+    "Enable JavaScript.",
+    "This site works better with JavaScript.",
+  ];
+
+  return (
+    <noscript style={{ pointerEvents: "none" }}>
+      {messages.map((message, i) => (
+        <NoScriptBanner key={i} i={i}>
+          {message}
+        </NoScriptBanner>
+      ))}
+    </noscript>
+  );
+};
+
+const noScripBannerAnimation = keyframes`
+  0% { top: -200% }
+  100% { top: 100% }
+`;
+
+const NoScriptBanner = ({
+  i,
+  children,
+}: {
+  i: number;
+  children: React.ReactNode;
+}) => {
+  const duration = Math.random() * 10 + 20;
+  const delay = i * 10;
+  const left = Math.random();
+  const right = Math.random();
+
+  return (
+    <NoScriptBannerWrapper $duration={duration} $delay={delay}>
+      <div style={{ flexGrow: left }} />
+      <NoScriptBannerContent>{children}</NoScriptBannerContent>
+      <div style={{ flexGrow: right }} />
+    </NoScriptBannerWrapper>
+  );
+};
+
+const NoScriptBannerWrapper = styled.div<{
+  $duration: number;
+  $delay: number;
+}>`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  z-index: 10000;
+  top: -1000px;
+
+  animation-name: ${noScripBannerAnimation};
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+
+  ${({ theme, $duration, $delay }) => css`
+    z-index: ${1000000 - Math.floor($duration * 100)};
+    animation-duration: ${$duration}s;
+    animation-delay: ${$delay}s;
+    padding: ${theme.spacing(2)}px;
+    border-radius: ${theme.spacing(1)}px;
+  `}
+`;
+
+const NoScriptBannerContent = styled.div`
+  background: white;
+  color: black;
+  font-weight: bold;
+  font-size: 2rem;
+  border: 1px solid #f008;
+  box-shadow: 1px 1px 1px 1px black;
+
+  ${({ theme }) => css`
+    padding: ${theme.spacing(2)}px;
+    border-radius: ${theme.spacing(1)}px;
+  `}
+`;
 
 export default App;
